a76a8d64f32868c9cdc66eeaf960f6ef64c9f948
Juan Pablo Escobedo Figueroa <jescobedo@tigo.com.gt>
2020-10-17 20:32:12 -0600

    Adds workspace directory

---
+++ README.md
@@ -0,0 +1,17 @@
+# Disaster Response Visual Pipeline
+
+This project consists of a machine learning pipeline that analizes disaster messages from Figure Eight, and classifies the messages.
+
+## Files
+Root directory contains jupyter notebooks with the pipeline.
+- ETL Pipeline Preparation.ipynb: data preparation for machine learning model
+- ML Pipeline Preparation.ipynb: machine learning pipeline
+- messages.csv: messages to analize
+- categories.cvs: categories to analize
+
+Workspace directory contains the same pipeline on python files
+- app/: web app to visualize the results
+- data/: data files to analize
+- models/: machine learning pipeline
+
+Open /workspace/README.md for more information about running the files on the workspace directory.
---
+++ workspace/models/train_classifier.py
@@ -0,0 +1,90 @@
+import sys
+import pandas as pd 
+from sqlalchemy import create_engine
+import pickle
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, f1_score
+from sklearn.model_selection import GridSearchCV
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('InsertTableName', con=engine)
+    X = df['message']
+    y = df.loc[:, 'related':'direct_report']
+    return X, y, y.columns
+
+
+def tokenize(text):
+    words = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    words = [w for w in words if w not in stopwords.words("english")]
+    words = [lemmatizer.lemmatize(w).lower().strip() for w in words ]
+    
+    return words
+
+
+def build_model():
+    return Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    for i in range(category_names.size):
+        print(category_names[i], ':', f1_score(Y_test.iloc[:, i], y_pred[:, i], average='weighted'))
+
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
